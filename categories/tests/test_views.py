bd09cd03d8ad3e5101bfe8bdd60476ed5050a01b
Shamimgardobuya <shamimobuya@gmail.com>
2025-08-19 21:32:59 +0300

    making mock function of token for reusability

--- categories/tests/test_views.py
+++ categories/tests/test_views.py
@@ -11,53 +11,42 @@ from categories.models import Category
 class CategoryCreationTests(APITestCase):
 
     def setUp(self):
-        Customer.objects.get_or_create(open_id='test-user-id')
+        self.customer, create_customer = Customer.objects.get_or_create(open_id='test-user-id')
         parent_category , create_parent_category = Category.objects.get_or_create(category_name='Bags')
         child_category = Category.objects.get_or_create(category_name="leather2 bag",parent=parent_category)
 
+    def mock_auth(self, mock_authenticate, mock_decode_token, permissions):
+        mock_authenticate.return_value = (self.customer, 'fake-token')
+        mock_decode_token.return_value = {
+            'sub': self.customer.open_id,
+            'permissions': permissions
+        }
+        return {'HTTP_AUTHORIZATION': 'Bearer fake-token'}
 
     def test_create_category_with_permission(self, mock_authenticate, mock_decode_token):
-
-        mock_authenticate.return_value = (Customer.objects.get(open_id='test-user-id'), 'fake-token')
-        mock_decode_token.return_value = {
-            'sub': 'test-user-id',
-            'scope': 'create:categories read:categories'
-        }
-        
+        headers = self.mock_auth(mock_authenticate, mock_decode_token,['create:categories' , 'read:categories'] )
         url = reverse('category_list')
-        headers = {'HTTP_AUTHORIZATION': 'Bearer fake-token'}
         data = {'category_name':  'Leather Bags', 'parent_category': 'Bags'}
         response = self.client.post(url, data, **headers, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
     def test_create_category_without_permission(self, mock_authenticate, mock_decode_token):
-        mock_authenticate.return_value = (Customer.objects.get(open_id='test-user-id'), 'fake-token')
-        
-        mock_decode_token.return_value = {
-            'sub': 'test-user-id',
-            'scope': 'read:categories'
-        }
+        headers = self.mock_auth(mock_authenticate, mock_decode_token,['read:categories'] )
         
         url = reverse('category_list')
-        headers = {'HTTP_AUTHORIZATION': 'Bearer fake-token'}
         data = {'category_name': 'Leather Bags', 'parent_category': 'bags'}
         response = self.client.post(url, data, **headers, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         
     def test_update_category_with_permission(self, mock_authenticate, mock_decode_token):
-        mock_authenticate.return_value = (Customer.objects.get(open_id='test-user-id'), 'fake-token')
-        mock_decode_token.return_value = {
-            'sub': 'test-user-id',
-            'scope': 'update:categories read:categories'
-        }
+        headers = self.mock_auth(mock_authenticate, mock_decode_token, ['update:categories', 'read:categories'])
         existing_category = Category.objects.get(category_name='leather2 bag')
         url = reverse('category_detail', args=[existing_category.id])
-        headers = { 'HTTP_AUTHORIZATION' : 'Bearer fake-token'}
         data = {'category_name': 'Cotton Bags', 'parent_category': 'Bags'}
         response = self.client.put(url, data,**headers)
         
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+        self.assertEqual(response.data['data']['category_name'],'Cotton Bags' )
         
